6f1076dadd830742d2ce37ca73bc1a17368dc3ce
BEANefiT <beanefit@BEANefiT.local>
2018-04-01 02:18:30 +0300

    began barray

---
+++ barray/barray.h
@@ -0,0 +1,24 @@
+#ifndef __BARRAY_H__
+#define __BARRAY_H__
+
+#include <cstddef>
+
+template <typename data_T>
+
+class barray
+{
+public:
+
+    explicit barray (size_t);
+    barray (const barray <data_T> &that);
+    barray (barray <data_T> &&that) noexcept;
+    ~barray ();
+
+private:
+
+    size_t      _capacity;
+    size_t      _size;
+    data_T*     _data;
+};
+
+#endif //__BARRAY_H__
