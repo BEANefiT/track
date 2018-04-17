34803b7131e0fddde71f8869c8a2862842613c54
BEANefiT <yunusov.rr@phystech.edu>
2018-04-18 02:33:51 +0300

    new() before throw

--- bexcept/bexcept.hpp
+++ bexcept/bexcept.hpp
@@ -1,7 +1,7 @@
 #ifndef __BEXCEPT_HPP__
 #define __BEXCEPT_HPP__
 
-class bexcept
+class bexcept: public std::exception
 {
 public:
     bexcept (const char*, size_t, bexcept*);
@@ -26,15 +26,23 @@ void bexcept :: what()
     for (size_t i = 0; i < _count; i++)
         std::cout << "\t\t";
 
-    std::cout << "In:\t" << _what << '\n';
+    std::cout << "In:\t" << _msg << '\n';
 
     if (_prev)
         _prev -> what();
+
+    delete this;
 }
 
 size_t bexcept :: count()
 {
     return _count;
 }
+
+#define bexcept_jump_next( msg )               \
+    auto e2 = new bexcept (msg, e -> count() + 1, e);    \
+    throw (e2);
+
+
 
 #endif //__BEXCEPT_HPP__
