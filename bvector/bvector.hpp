fd5d6c554b915f73ecdc0256b5044e182c3cccf6
BEANefiT <yunusov.rr@phystech.edu>
2018-04-21 12:45:14 +0300

    protection and bexcept compatibility

--- bvector/bvector.hpp
+++ bvector/bvector.hpp
@@ -1,16 +1,15 @@
 #ifndef __BVECTOR_HPP__
 #define __BVECTOR_HPP__
 
-#include <new>
-#include <cstddef>
+#include "../bexcept/bexcept.hpp"
 
 template <typename data_T>
-
 class bvector
 {
 public:
 
-    explicit bvector (size_t capacity = 1);
+    bvector();
+    explicit bvector (size_t);
     bvector (const bvector <data_T> &that);
     bvector (size_t, const data_T &value);
     ~bvector ();
@@ -32,20 +31,36 @@ private:
 };
 
 template <typename data_T>
+bvector <data_T> :: bvector():
+        _size (0),
+        _capacity (0),
+        _data (nullptr)
+{}
+
+template <typename data_T>
 bvector <data_T> :: bvector (size_t capacity):
         _size (0),
         _capacity (capacity)
 {
     _data = new (std::nothrow) data_T [_capacity];
+
+    if (_data == nullptr)
+    {
+        bexcept_throw ("can't allocate memory");
+    }
 }
 
 template <typename data_T>
 bvector <data_T> :: bvector (const bvector <data_T> &that):
-
         _size (that._size),
         _capacity (that._capacity)
 {
     _data = new (std::nothrow) data_T [_capacity];
+
+    if (_data == nullptr)
+    {
+        bexcept_throw ("can't allocate memory");
+    }
 
     for (int i = 0; i < _capacity; i++)
         _data[i] = that._data[i];
@@ -58,6 +73,11 @@ bvector <data_T> :: bvector (size_t capacity, const data_T &value):
 {
     _data = new (std::nothrow) data_T [_capacity];
 
+    if(_data == nullptr)
+    {
+        bexcept_throw ("can't allocate memory");
+    }
+
     for (int i = 0; i < _capacity; i++)
         _data[i] = value;
 }
@@ -67,6 +87,8 @@ bvector <data_T> :: ~bvector ()
 {
     if (_data != nullptr)
         delete _data;
+
+    _data = nullptr;
 }
 
 template <typename data_T>
@@ -80,7 +102,17 @@ void bvector <data_T> :: swap (bvector <data_T> &that)
 template <typename data_T>
 void bvector <data_T> :: resize (size_t new_capacity)
 {
+    if (new_capacity < _size)
+    {
+        bexcept_throw ("loss of data, because new_capacity < _size");
+    }
+
     data_T* new_data = new (std::nothrow) data_T [new_capacity];
+
+    if (new_data == nullptr)
+    {
+        bexcept_throw ("can't allocate memory");
+    }
 
     for (size_t i = 0; i < _size; i++)
         new_data[i] = _data[i];
@@ -94,7 +126,11 @@ template <typename data_T>
 void bvector <data_T> :: push_back (const data_T &value)
 {
     if (_size >= _capacity)
-        resize (2 * _capacity);
+    {
+        try{ resize(2 * _capacity + 1); }
+
+        catch (bexcept* e){ bexcept_throw_without_msg (e); }
+    }
 
     _data[_size++] = value;
 }
@@ -102,31 +138,46 @@ void bvector <data_T> :: push_back (const data_T &value)
 template <typename data_T>
 data_T bvector <data_T> :: pop_back ()
 {
-    if (_size > 0)
-        return _data[--_size];
+    if (_size <= 0)
+    {
+        bexcept_throw ("bvector is empty");
+    }
+
+    return _data[--_size];
 }
 
 template <typename data_T>
-data_T &bvector <data_T> :: operator [] (size_t index)
+data_T &bvector <data_T> :: operator[] (size_t index)
 {
-    if (index < _size)
-        return _data[index];
+    if (index >= _capacity)
+    {
+        bexcept_throw ("index >= _capacity");
+    }
+
+    return _data[index];
 }
 
 template <typename data_T>
-bvector <data_T> &bvector <data_T> :: operator = (const bvector <data_T> &that)
+bvector <data_T> &bvector <data_T> :: operator= (const bvector <data_T> &that)
 {
-    bvector <data_T> tmp (that);
-    swap (tmp);
+    try
+    {
+        bvector<data_T> tmp(that);
+        swap(tmp);
+    }
+
+    catch (bexcept *e)
+    {
+        bexcept_throw_without_msg (e);
+    }
 }
 
 template <typename data_T>
-bvector <data_T> &bvector <data_T> :: operator = (bvector &&that) noexcept
+bvector <data_T> &bvector <data_T> :: operator= (bvector &&that) noexcept
 {
-    std::swap (_size, that._size);
-    std::swap (_capacity, that._capacity);
-    std::swap (_data, that._data);
+    try{ swap (that); }
 
+    catch (bexcept* e){ bexcept_throw_without_msg (e); }
 }
 
 template <typename data_T>
