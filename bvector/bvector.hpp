155d62636ae2a49481ec2391c16ca35648c6b19d
BEANefiT <yunusov.rr@phystech.edu>
2018-04-21 12:08:21 +0300

    some changes for this time

--- bvector/bvector.hpp
+++ bvector/bvector.hpp
@@ -2,7 +2,34 @@
 #define __BVECTOR_HPP__
 
 #include <new>
-#include "bvector.h"
+#include <cstddef>
+
+template <typename data_T>
+
+class bvector
+{
+public:
+
+    explicit bvector (size_t capacity = 1);
+    bvector (const bvector <data_T> &that);
+    bvector (size_t, const data_T &value);
+    ~bvector ();
+
+    void                swap (bvector <data_T> &that);
+    void                resize (size_t);
+    void                push_back (const data_T &value);
+    data_T              pop_back ();
+    data_T              &operator [] (size_t index);
+    bvector <data_T>    &operator = (const bvector <data_T> &that);
+    bvector <data_T>    &operator = (bvector &&that) noexcept;
+    size_t              size ();
+
+private:
+
+    size_t      _size;
+    size_t      _capacity;
+    data_T*     _data;
+};
 
 template <typename data_T>
 bvector <data_T> :: bvector (size_t capacity):
