142e7bf4cf4a1e5000975c28821ddf20c7a3cae6
BEANefiT <yunusov.rr@phystech.edu>
2018-04-21 16:14:43 +0300

    began <bool> bvector specialization

--- bvector/bvector.hpp
+++ bvector/bvector.hpp
@@ -85,8 +85,11 @@ bvector <data_T> :: bvector (size_t capacity, const data_T &value):
 template <typename data_T>
 bvector <data_T> :: ~bvector ()
 {
+    for (int i = 0; i < _capacity; i++)
+        _data [i].~data_T();
+
     if (_data != nullptr)
-        delete _data;
+        delete [] _data;
 
     _data = nullptr;
 }
@@ -114,7 +117,7 @@ void bvector <data_T> :: resize (size_t new_capacity)
         bexcept_throw ("can't allocate memory");
     }
 
-    for (size_t i = 0; i < _size; i++)
+    for (size_t i = 0; i < _capacity; i++)
         new_data[i] = _data[i];
 
     delete _data;
@@ -143,7 +146,10 @@ data_T bvector <data_T> :: pop_back ()
         bexcept_throw ("bvector is empty");
     }
 
-    return _data[--_size];
+    auto tmp = _data [--_size];
+    _data [size].~data_T();
+
+    return tmp;
 }
 
 template <typename data_T>
@@ -184,6 +190,129 @@ template <typename data_T>
 size_t bvector <data_T> :: size ()
 {
     return _size;
+}
+
+template <>
+class bvector <bool>
+{
+public:
+
+    bvector();
+    explicit bvector (size_t);
+    bvector (const bvector &that);
+    bvector (size_t, const bool &value);
+    ~bvector ();
+
+    void                swap (bvector &that);
+    void                resize (size_t);
+    void                push_back (const bool &value);
+    bool                pop_back ();
+    proxy_t             operator [] (size_t index);
+    bvector             &operator = (const bvector &that);
+    bvector             &operator = (bvector &&that) noexcept;
+    size_t              size ();
+
+private:
+
+    size_t      _size;
+    size_t      _capacity;
+    char*       _data;
+};
+
+bvector <bool> :: bvector():
+        _size (0),
+        _capacity (0),
+        _data (nullptr)
+{}
+
+bvector <bool> :: bvector (size_t capacity):
+        _size (0),
+        _capacity (8 * (capacity / 8 + (capacity % 8) / (capacity % 8)))
+{
+    _data = new (std::nothrow) char [_capacity / 8];
+
+    if (_data == nullptr)
+    {
+        bexcept_throw ("can't allocate memory");
+    }
+}
+
+bvector <bool> :: bvector (const bvector &that):
+        _size (that._size),
+        _capacity (that._capacity)
+{
+    _data = new (std::nothrow) char [_capacity / 8];
+
+    if (_data == nullptr)
+    {
+        bexcept_throw ("can't allocate memory");
+    }
+
+    for (int i = 0; i < _capacity / 8; i++)
+        _data [i] = that._data [i];
+}
+
+bvector <bool> :: bvector (size_t capacity, const bool &value):
+        _capacity (8 * (capacity / 8 + (capacity % 8) / (capacity % 8))),
+        _size (_capacity)
+{
+    _data = new (std::nothrow) char [_capacity / 8];
+
+    if (_data == nullptr)
+    {
+        bexcept_throw ("can't allocate memory");
+    }
+
+    if (value == 0)
+        for (int i = 0; i < _capacity / 8; i++)
+            _data [i] = 0;
+
+    if (value == 1)
+        for (int i = 0; i < _capacity / 8; i++)
+            _data [i] = 0xff;
+}
+
+bvector <bool> :: ~bvector ()
+{
+    if (_data != nullptr)
+    {
+        for (int i = 0; i < _capacity / 8; i++)
+            _data[i].~proxy_t()
+
+        delete[] _data;
+
+        _data = nullptr;
+    }
+}
+
+void bvector <bool> :: swap (bvector &that)
+{
+    std::swap (_data, that._data);
+    std::swap (_size, that._size);
+    std::swap (_capacity, that._capacity);
+}
+
+void bvector <bool> :: resize (size_t new_capacity)
+{
+    if (new_capacity < _size)
+    {
+        bexcept_throw ("loss of data, because new_capacity < _size");
+    }
+
+    _capacity = 8 * (new_capacity / 8 + (new_capacity % 8) / (new_capacity % 8));
+
+    data_T* new_data = new (std::nothrow) char [_capacity / 8];
+
+    if (new_data == nullptr)
+    {
+        bexcept_throw ("can't allocate memory");
+    }
+
+    for (size_t i = 0; i < _capacity / 8; i++)
+        new_data[i] = _data[i];
+
+    delete _data;
+    _data = new_data;
 }
 
 #endif //__BVECTOR_HPP__
