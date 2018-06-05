edeaf2d95253f454f8d261a11e382e55b9d8b857
BEANefiT <beanefit@BEANefiT.local>
2018-06-06 00:28:02 +0300

    prototypes of funcs

--- game/player.hpp
+++ game/player.hpp
@@ -1,7 +1,10 @@
 #ifndef __PLAYER_HPP__
 #define __PLAYER_HPP__
 
-#include "gameobj.hpp"
+#include "engine/gameobj.hpp"
+
+#define ISKEY sf::Keyboard::isKeyPressed
+#define KEY sf::Keyboard::Key
 
 class player: public gameobj
 {
@@ -10,82 +13,78 @@ class player: public gameobj
         float       _diag_speed;
 
     public:
-        player (sf::Texture* t, float x, float y):
-            gameobj (t, 96, 96, x, y, 0, 0, 0, 0),
+        player (sf::Texture& t, float x, float y):
+            gameobj (t, x, y, 96, 96, 32, 32, 2, 8, 0.01),
             _default_speed (0.1)
         {
             _diag_speed  = _default_speed / 1.41;
         };
 
+        void check();
+    
         void move (float time) override
         {
+            check();
+            
             if (_vx == 0 && _vy == 0)
             {
-                _sprite.setTextureRect (sf::IntRect (2 * _width, _dir * _height, _width, _height));
+                stay();
+                
                 return;
             }
+            
+            struct vector df (_vx * time, _vy * time);
+            
+            graphobj::  move (df);
+            physobj::   move (df);
 
-            _x += _vx * time;
-            _y += _vy * time;
-
-            _sprite.move (_vx * time, _vy * time);
-            _sprite.setTextureRect (sf::IntRect (   _width * (int)_frame_num,
-                                                    _dir * _height,
-                                                    _width,
-                                                    _height                 ));
             _vx = 0;
             _vy = 0;
-
-            _frame_num += 0.012;
-
-            if (_frame_num >= 8)
-                _frame_num = 1;
         }
 
-        void draw (sf::RenderWindow* window)
+        void draw (sf::RenderWindow& window) override
         {
-            window -> draw (_sprite);
-        }
-
-        virtual void check (enum sf::Keyboard::Key w, enum sf::Keyboard::Key a,
-                            enum sf::Keyboard::Key s, enum sf::Keyboard::Key d) override
-        {
-            if (iskey (a))
-            {
-                if (iskey (w))  { speed (left_up,   _diag_speed); return; }
-                    
-                if (iskey (s))  { speed (left_down, _diag_speed); return; }
-
-                speed (left, _default_speed);
-
-                return;
-            }
-
-            if (iskey (d))
-            {
-                if (iskey (w))  { speed (right_up,   _diag_speed); return; }
-                    
-                if (iskey (s))  { speed (right_down, _diag_speed); return; }
-
-                speed (right, _default_speed);
-
-                return;
-            }
-
-            if (iskey (w))
-            {
-                speed (up, _default_speed);
-
-                return;
-            }
-
-            if (iskey (s))
-            {
-                speed (down, _default_speed);
-
-                return;
-            }
+            graphobj::draw (window);
         }
 };
+
+void player::check()
+{
+    if (ISKEY (KEY::A))
+    {
+        if (ISKEY (KEY::W))  { set_speed (left_up,   _diag_speed); return; }
+        
+        if (ISKEY (KEY::S))  { set_speed (left_down, _diag_speed); return; }
+        
+        set_speed (left, _default_speed);
+        
+        return;
+    }
+    
+    if (ISKEY (KEY::D))
+    {
+        if (ISKEY (KEY::W))  { set_speed (right_up,   _diag_speed); return; }
+        
+        if (ISKEY (KEY::S))  { set_speed (right_down, _diag_speed); return; }
+        
+        set_speed (right, _default_speed);
+        
+        return;
+    }
+    
+    if (ISKEY (KEY::W))
+    {
+        set_speed (up, _default_speed);
+        
+        return;
+    }
+    
+    if (ISKEY (KEY::S))
+    {
+        set_speed (down, _default_speed);
+        
+        return;
+    }
+}
 
 #endif //__PLAYER_HPP__
