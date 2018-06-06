bb080e5b250c43adaab1c69fe35cdf71eae2de6f
BEANefiT <beanefit@BEANefiT.local>
2018-06-06 11:57:44 +0300

    some physobj funcs' realisation

--- game/engine/physobj.hpp
+++ game/engine/physobj.hpp
@@ -35,5 +35,60 @@ public:
     void    change_acceleration (struct vector);
 };
 
+physobj::physobj (float x, float y, float width, float height):
+    _x      (x),
+    _y      (y),
+    _width  (width),
+    _height (height)
+{}
+
+physobj::physobj (float x, float y, float width, float height, float vx, float vy, float ax, float ay):
+    _x      (x),
+    _y      (y),
+    _width  (width),
+    _height (height),
+    _vx     (vx),
+    _vy     (vy),
+    _ax     (ax),
+    _ay     (ay)
+{}
+
+#define SET( name, param1, param2, v, operand1, operand2)   \
+    void    physobj:: name (float param1, float param2)     \
+    {                                                       \
+        operand1 = param1;                                  \
+        operand2 = param2;                                  \
+    }                                                       \
+                                                            \
+    void    physobj:: name (struct vector v)                \
+    {                                                       \
+        operand1 = v##.x;                                   \
+        operand2 = v##.y;                                   \
+    }
+
+#define CHANGE( name, param1, param2, v, operand1, operand2)\
+    void    physobj:: name (float param1, float param2)     \
+    {                                                       \
+        operand1 += param1;                                 \
+        operand2 += param2;                                 \
+    }                                                       \
+                                                            \
+    void    physobj:: name (struct vector v)                \
+    {                                                       \
+        operand1 += v##.x;                                  \
+        operand2 += v##.y;                                  \
+    }
+
+SET     (set_pos, x,  y,  r,  _x, _y);
+CHANGE  (move,    dx, dy, dr, _x, _y);
+
+SET     (set_speed,    vx,  vy,  v,  _vx, _vy);
+CHANGE  (change_speed, dvx, dvy, dv, _vx, _vy);
+
+SET     (set_acceleration,    ax,  ay,  a,  _ax, _ay);
+CHANGE  (change_acceleration, dax, day, da, _ax, _ay);
+
+#undef SET
+#undef CHANGE
 
 #endif //__PHYSOBJ_HPP__
